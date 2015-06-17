f0abbbf201f12e4d5bf302c738f19796d26c96c4
tdeheurles <tdeheurles@gmail.com>
2015-06-17 23:55:51 +0200

    Corrected build path

--- main.sh
+++ main.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # import names
-. ./jvm-tools/release.cfg
+. ./release.cfg
 artifact_name="gcr.io/$projectid/$servicename"
 artifact_tag="$artifact_name:$servicemajor.$serviceminor.$BUILD_NUMBER"
 
 # Build
-docker build -t $artifact_name ./jvm-tools/
+docker build -t $artifact_name .
 docker tag $artifact_name $artifact_tag
 
 # Push to Google Cloud Engine
